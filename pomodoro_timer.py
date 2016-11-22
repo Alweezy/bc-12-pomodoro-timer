56479e790c66c9d0e382cbb61b7d8da2c04f4ca5
Alvin <alvinmutisya@gmail.com>
2016-11-22 21:35:10 +0300

    Edit the start time function to use defaults

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,11 +1,12 @@
 import datetime
+import pygame
 import time
 import sys
 class PomoDoroTimer(object):
 	def __init__(self):
 		self.cycle_start = datetime.datetime.now()
 
-	def start_timer(self, cycle_duration, number_of_cycles=4):
+	def start_timer(self, cycle_duration=25, number_of_cycles=4):
 		"""Function starts a pomodoro timer and records the task title,
 		and start_time and date.
 		It also plays a sound bell at the end of a pomodoro cycle.
@@ -15,22 +16,21 @@ class PomoDoroTimer(object):
 		print self.cycle_start
 	 	end_of_cycle = self.config_time(cycle_duration)
 	 	for cycle in range(0, number_of_cycles):
-			for remaining in range(cycle_duration *60, 0, -1):
+			for rem_time in range(cycle_duration *60, 0, -1):
 			    sys.stdout.write("\r")
-			    sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+			    sys.stdout.write("{:2d} seconds remaining.".format(rem_time)) 
 			    sys.stdout.flush()
 			    time.sleep(1)
-			if remaining is 1 and self.config_sound(True):
+			if rem_time is 1 and self.config_sound(True):
 				sys.stdout.write("\r*****PROMODORO CYCLE IS OVER*******\n")
-				import pygame
-				pygame.init()
-				pygame.mixer.music.load("bell.wav")
-				pygame.mixer.music.play()
-				pygame.event.wait()
-				print "going on a short break\n"
+				# pygame.init()
+				# pygame.mixer.music.load("bell.wav")
+				# pygame.mixer.music.play()
+				# pygame.event.wait()
 				self.config_short_break()
-			else:
+			elif rem_time is 1 and self.config_sound(False):
 				sys.stdout.write("\rpomodoro cycle is over but alarm is off\n")
+				self.config_short_break()
 
 	def config_time(self, cycle_duration):
 		"""Function sets the time duration for a particular promodoro,
@@ -52,11 +52,14 @@ class PomoDoroTimer(object):
 		if no time value is given, then the default time value is used.
 
 		"""
+		print "going on a short break\n"
 		for remaining in range(shrt_break_duration * 60, 0, -1):
 		    sys.stdout.write("\r")
 		    sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
 		    sys.stdout.flush()
 		    time.sleep(1)
+		print "\n"
+		print "********END OF SHORT BREAK, GOING BACK TO CYCLE*********\n"
 
 
 	def config_long_break(self, lng_break_duration = 1):
@@ -94,5 +97,5 @@ class PomoDoroTimer(object):
 
 
 
-my_pro = PomoDoroTimer()
-print my_pro.start_timer(1 )
+# my_pro = PomoDoroTimer()
+# print my_pro.start_timer(1)
