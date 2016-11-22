2b325d70219807e7b443a68c538216b703912aa9
Alvin <alvinmutisya@gmail.com>
2016-11-22 17:05:48 +0300

    Alter some config functions

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -1,32 +1,36 @@
 import datetime
+import time
+import sys
 class PomoDoroTimer(object):
 	def __init__(self):
 		self.cycle_start = datetime.datetime.now()
 
-	def start_timer(self, cycle_duration):
+	def start_timer(self, cycle_duration, number_of_cycles=4):
 		"""Function starts a pomodoro timer and records the task title,
 		and start_time and date.
 		It also plays a sound bell at the end of a pomodoro cycle.
 
 		"""
+
 		print self.cycle_start
 	 	end_of_cycle = self.config_time(cycle_duration)
-	 	import time
-		import sys
-		for remaining in range(cycle_duration *60, 0, -1):
-		    sys.stdout.write("\r")
-		    sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
-		    sys.stdout.flush()
-		    time.sleep(1)
-		if remaining is 1 and self.config_sound(True):
-			sys.stdout.write("\rpomodoro cycle is over.......\n")
-			# import pygame
-			# pygame.init()
-			# pygame.mixer.music.load("bell.wav")
-			# pygame.mixer.music.play()
-			# pygame.event.wait()
-		else:
-			sys.stdout.write("\rpomodoro cycle is over but alarm is off\n")
+	 	for cycle in range(0, number_of_cycles):
+			for remaining in range(cycle_duration *60, 0, -1):
+			    sys.stdout.write("\r")
+			    sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+			    sys.stdout.flush()
+			    time.sleep(1)
+			if remaining is 1 and self.config_sound(True):
+				sys.stdout.write("\r*****PROMODORO CYCLE IS OVER*******\n")
+				import pygame
+				pygame.init()
+				pygame.mixer.music.load("bell.wav")
+				pygame.mixer.music.play()
+				pygame.event.wait()
+				print "going on a short break\n"
+				self.config_short_break()
+			else:
+				sys.stdout.write("\rpomodoro cycle is over but alarm is off\n")
 
 	def config_time(self, cycle_duration):
 		"""Function sets the time duration for a particular promodoro,
@@ -43,19 +47,29 @@ class PomoDoroTimer(object):
 			self.default_time = cycle_stop
 			return cycle_stop
 
-	def config_short_break(self, shrt_break_duration=5):
+	def config_short_break(self, shrt_break_duration=1):
 		"""Function sets a short break in between a pomodoro task,
 		if no time value is given, then the default time value is used.
 
 		"""
-		return shrt_break_duration
+		for remaining in range(shrt_break_duration * 60, 0, -1):
+		    sys.stdout.write("\r")
+		    sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+		    sys.stdout.flush()
+		    time.sleep(1)
 
 
-	def config_long_break(self, lng_break_duration = 15):
+	def config_long_break(self, lng_break_duration = 1):
 		"""Function sets the duration for the long break after a pomodoro
 		task, if no time value is given, then default duration is used.
 		"""
-		return lng_break_duration
+		for remaining in range(lng_break_duration * 60, 0, -1):
+			sys.stdout.write("\r")
+			sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+			sys.stdout.flush()
+			time.sleep(1)
+
+
 
 	def config_sound(self, toggle=False):
 		"""Function turns sound notification on/off
@@ -81,4 +95,4 @@ class PomoDoroTimer(object):
 
 
 my_pro = PomoDoroTimer()
-print my_pro.config_short_break()
+print my_pro.start_timer(1 )
