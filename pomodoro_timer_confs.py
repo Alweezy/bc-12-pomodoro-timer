51b329cdc95e4f9cac151fa0685a081d85f9b834
Alvin <alvinmutisya@gmail.com>
2016-11-23 22:05:47 +0300

    Add config modules to work with docopt

--- pomodoro_timer_confs.py
+++ pomodoro_timer_confs.py
@@ -4,97 +4,63 @@ import time
 import sys
 class PomoDoroTimer(object):
 	def __init__(self):
-		pass
+		self.cycle_length = 25
+		self.short_break_length = 5
+		self.long_break_length = 15
+		self.sound = True
 
-	def start_timer(self, task_name):
+	def start(self, task_name):
 		"""Function starts a pomodoro timer and records the task title,
 		and start_time and date.
 		It also plays a sound bell at the end of a pomodoro cycle.
 
 		"""
-		task = str(raw_input("Enter the name of your task: "))
-		if task:
-			try:
-				task_name = task.upper()
-				task_time = datetime.datetime.now().time()
-				task_date = datetime.date.today()
-				print task_time
-				return ""
-			except ValueError:
-				print "every task should have a name"
-				return ""
-	def config_time(self, cycle_duration):
+		pass:
+
+	def config_time(self, cycle_length):
 		"""Function sets the time duration for a particular promodoro cycle,
 		if no time is given, then default time is used.
 
 		"""
-		settings = raw_input("Enter time for a pomodoro cycle?[y/n]:")
-		cycle_settings = settings.lower()
-		if settings == "":
-			print "The default duration [25] will be used...."
-			cycle_duration == 25;
-			return cycle_duration
-		if settings:
-			duration = raw_input("Specify duration in minutes: ")
+		if cycle_length:
 			try:
-				cycle_duration = (int(duration))
-				return cycle_duration
+				self.cycle_length = int(cycle_length)
 			except ValueError:
-				print "Invalid inputs, try again"
+				print 'Only integers accepted'
+		return 
 
-	def config_short_break(self, shrt_break_duration):
+	def config_short_break(self, short_break_length):
 		"""Function sets a short break in between a pomodoro task,
 		if no time value is given, then the default time value is used.
 
 		"""
-		settings = raw_input("Enter time for a short break?[y/n]:")
-		brk_settings = settings.lower()
-		if settings == "":
-			print "The default duration [5] will be used...."
-			shrt_break_duration == 5;
-			return shrt_break_duration
-		if settings:
-			duration = raw_input("Specify duration in minutes: ")
+		if short_break_length:
 			try:
-				shrt_break_duration = (int(duration))
-				return shrt_break_duration
+				self.short_break_length = int(short_break_length)
 			except ValueError:
-				print "Invalid inputs, try again"
-		
+				print 'Only integers accepted'
+		return 
 
-	def config_long_break(self, lng_break_duration):
+	def config_long_break(self, long_break_length):
 		"""Function sets the duration for the long break after a pomodoro
 		task, if no time value is given, then default duration is used.
 		"""
-		settings = raw_input("Enter time for a long break?[y/n]:")
-		brk_settings = settings.lower()
-		if settings == "":
-			print "The default duration [15] will be used...."
-			lng_break_duration == 5;
-			return lng_break_duration
-		if settings:
-			duration = raw_input("Specify duration in minutes: ")
+		if long_break_length:
 			try:
-				lng_break_duration = (int(duration))
-				return lng_break_duration
+				self.long_break_length = int(long_break_length)
 			except ValueError:
-				print "Invalid inputs, try again"
+				print 'Only integers accepted'
+		return 
+
 		
-	def config_sound(self, toggle):
+	def config_sound(self, sound):
 		"""Function turns sound notification on/off
 
 		"""
-		settings = raw_input("Do you want turn Alarm audio ON? [y/n] ")
-		toggle_settings = settings.lower()
-		if settings == "":
-			print "The default, [OFF] will be used...."
-			toggle = False			
-			return toggle
-		if settings:
-			pygame.init()
-			pygame.mixer.music.load("bell_true.mp3")
-			pygame.mixer.music.play()
-			time.sleep(4)
-
-my_pro = PomoDoroTimer()
-print my_pro.config_sound(1)
+		if sound.capitalize() == 'True':
+			self.sound = True
+		elif sound.capitalize() == 'False':
+			self.sound = False
+		else:
+			print 'Only bool variable, i.e True or False'
+		return 
