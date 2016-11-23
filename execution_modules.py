917f9a6d9bf0c1fbcb1523e4458c7e29f6234f0d
Alvin <alvinmutisya@gmail.com>
2016-11-23 22:56:17 +0300

    Add functionality to the modules

--- execution_modules.py
+++ execution_modules.py
@@ -1,8 +1,28 @@
+import time
+import sys
 def cycle(length):
-	pass
+	for rem_time in range(length *60, 0, -1):
+		sys.stdout.write("\r")
+		sys.stdout.write("{:2d} seconds remaining.".format(rem_time))
+		sys.stdout.flush()
+		time.sleep(1)
+
 def short_break(length):
-	pass
+	for rem_time in range(length *60, 0, -1):
+		sys.stdout.write("\r")
+		sys.stdout.write("{:2d} seconds remaining.".format(rem_time))
+		sys.stdout.flush()
+		time.sleep(1)
+
 def long_break(length):
-	pass
+	for rem_time in range(length *60, 0, -1):
+	    sys.stdout.write("\r")
+	    sys.stdout.write("{:2d} seconds remaining.".format(rem_time))
+	    sys.stdout.flush()
+	    time.sleep(1)
+
 def bell(play_sound):
-	pass
+	pygame.init()
+	pygame.mixer.music.load("bell_true.mp3")
+	pygame.mixer.music.play()
+	time.sleep(4)
