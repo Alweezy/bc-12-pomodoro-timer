27497074917f05a0b142d6c1e5ba81943bceaf41
Alvin <alvinmutisya@gmail.com>
2016-11-24 09:33:30 +0300

    change duration values to seconds evaluation purposes

--- execution_modules.py
+++ execution_modules.py
@@ -1,21 +1,21 @@
 import time
 import sys
 def cycle(length):
-	for rem_time in range(length *60, 0, -1):
+	for rem_time in range(length, 0, -1):
 		sys.stdout.write("\r")
 		sys.stdout.write("{:2d} seconds remaining.".format(rem_time))
 		sys.stdout.flush()
 		time.sleep(1)
 
 def short_break(length):
-	for rem_time in range(length *60, 0, -1):
+	for rem_time in range(length, 0, -1):
 		sys.stdout.write("\r")
 		sys.stdout.write("{:2d} seconds remaining.".format(rem_time))
 		sys.stdout.flush()
 		time.sleep(1)
 
 def long_break(length):
-	for rem_time in range(length *60, 0, -1):
+	for rem_time in range(length, 0, -1):
 	    sys.stdout.write("\r")
 	    sys.stdout.write("{:2d} seconds remaining.".format(rem_time))
 	    sys.stdout.flush()
